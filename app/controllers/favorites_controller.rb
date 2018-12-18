263aa3fd5fedc7830ff628ab97eb3e150c5c2440
Noguchi Norimasa <norimasanoguchi@gmail.com>
2018-12-18 23:26:30 +0000

    お気に入り投稿解除機能の不具合修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,6 @@
 class FavoritesController < ApplicationController
  
   def create
-    binding.pry
     favorite = current_user.favorites.create(post_id: params[:post_id])
     redirect_to posts_url, notice: "#{favorite.user.name}さんの写真をお気に入りに登録しました"
   end
@@ -9,6 +8,6 @@ class FavoritesController < ApplicationController
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to posts_url, notice: "#{favorite.user.name}さんの写真をお気に入りを解除しました"
-　end
- end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def favorites
+    @user = User.find(params[:id])
+    @favorites = current_user.favorite_posts
+  end
+  
   
   private
   
