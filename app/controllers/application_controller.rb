44d7b9ec14aa11529397a44bbae8e125bb8b4f40
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-01-16 23:32:11 +0000

    フィルタによるログアウト時アクセス制限+view側でのアクセス制限除去

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  add_flash_types :success, :info, :warning, :danger
+  before_action :require_login
+  
+  private
+  
+  def require_login
+    unless logged_in?
+      flash[:danger] = "アクセスするためにはログインしてください"
+      redirect_to new_session_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  
   def new
-    @user = User.new
+   @user = User.new
   end
   
   def create
