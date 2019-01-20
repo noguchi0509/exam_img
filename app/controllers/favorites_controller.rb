5a1c1c49f4999db98c429c5dc6fc13b9c093ffa0
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-01-20 03:30:16 +0000

    flashが二重になる問題の解決と、お気に入り設定時にflashに表示される名前修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
  
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to posts_url, notice: "#{favorite.user.name}さんの写真をお気に入りに登録しました"
+    redirect_to posts_url, success: "#{favorite.post.user.name}さんの写真をお気に入りに登録しました"
   end
   
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to posts_url, notice: "#{favorite.user.name}さんの写真をお気に入りを解除しました"
+    redirect_to posts_url, success: "#{favorite.post.user.name}さんの写真をお気に入りを解除しました"
   end
 
 end
