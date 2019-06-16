0f791905b4180e5c147323a71c31db7c39ff52a5
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-06-16 07:06:41 +0000

    Modify SSH key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 # server '3.113.72.205', user: 'app', roles: %w{app db web}
 # set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
 server '52.193.249.6', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
+set :ssh_options, keys: '/home/ubuntu/.ssh/id_rsa'
+# set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
 # set :ssh_options, keys: '/home/app/.ssh/id_rsa'
