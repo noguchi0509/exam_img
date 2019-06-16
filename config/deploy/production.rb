afebed5aee2b4f336db4ec3de9f7d87a5de1b208
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-06-16 04:34:31 +0000

    init unicorn4

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 # server '3.113.72.205', user: 'app', roles: %w{app db web}
 # set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
 server '3.113.186.64', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/home/app/.ssh/id_rsa'
+set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
+# set :ssh_options, keys: '/home/app/.ssh/id_rsa'
