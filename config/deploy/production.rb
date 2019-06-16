e6956f262989191d53cab0a0493349b3f0d193d7
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-06-16 06:57:56 +0000

    Configure AWS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # server '3.113.72.205', user: 'app', roles: %w{app db web}
 # set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
-server '3.113.186.64', user: 'app', roles: %w{app db web}
+server '52.193.249.6', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/home/ubuntu/workspace/id_rsa'
 # set :ssh_options, keys: '/home/app/.ssh/id_rsa'
